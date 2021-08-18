f7617dd538b947b5dc05dcd3636ce3f96a556e3b
Zacharie Corniere <zacharie@corniere.me>
2021-08-18 17:55:18 +0200

    example(simple): added a more simple logger example

---
+++ example/example_simple.cpp
@@ -0,0 +1,23 @@
+#define LOGGER_EXTERN_DECLARATION
+#include <Logger.hpp>
+#include <iostream>
+#include <unistd.h>
+
+Logger logger(std::cout);
+
+int main(void)
+{
+    logger.start();
+
+    logger.err("Test") << "this is an error message, will be printed in red";
+    logger.endl();
+
+    LOGGER_WARN << "This is a warning, that will print the current position";
+    LOGGER_ENDL;    // same as logger.endl();
+
+    logger.info() << "this is an information message";
+    logger.endl();
+
+    logger.debug("debug") << "this is a debug message";
+    logger.endl();
+}
