35571b47a3a12daccde6a4712cb6048e1d50bd80
Zacharie Corniere <zacharie@corniere.me>
2021-08-18 17:58:21 +0200

    example(level): added example for logger level control

---
+++ example/example_level.cpp
@@ -0,0 +1,52 @@
+#define LOGGER_EXTERN_DECLARATION
+#include <Logger.hpp>
+#include <iostream>
+#include <unistd.h>
+
+Logger logger(std::cout);
+
+void printMessage(Logger::Level level)
+{
+    logger.setLevel(level);
+    logger.debug() << "This is a debug message";
+    LOGGER_ENDL;
+
+    logger.info() << "This is an information message";
+    LOGGER_ENDL;
+
+    logger.warn() << "This is a warning message";
+    LOGGER_ENDL;
+
+    logger.err() << "This is an error message";
+    LOGGER_ENDL;
+
+    logger.msg() << "This is a message";
+    LOGGER_ENDL;
+
+    LOGGER_ENDL;
+}
+
+int main(void)
+{
+    logger.start();
+
+    logger.msg() << "Debug | Info | Warn | Error | Message";
+    LOGGER_ENDL;
+    printMessage(Logger::Level::Debug);
+
+    logger.msg() << "Info | Warn | Error | Message";
+    LOGGER_ENDL;
+    printMessage(Logger::Level::Info);
+
+    logger.msg() << " Warn | Error | Message";
+    LOGGER_ENDL;
+    printMessage(Logger::Level::Warn);
+
+    logger.msg() << "Error | Message";
+    LOGGER_ENDL;
+    printMessage(Logger::Level::Error);
+
+    logger.msg() << "Message";
+    LOGGER_ENDL;
+    printMessage(Logger::Level::Message);
+}
