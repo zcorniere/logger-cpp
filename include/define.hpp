86a0e3afbd8c40251f3a3b4787bb9fdc6a93473a
Zacharie Corniere <zacharie@corniere.me>
2022-05-06 19:49:26 +0200

    chore(Terminal): move the define to a dedicated header

---
+++ include/define.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#if defined(_WIN32) || defined(_WIN64)
+    #define TERMINAL_TARGET_WINDOWS
+#elif defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
+    #define TERMINAL_TARGET_POSIX
+#endif
+
+#if defined(TERMINAL_TARGET_POSIX)
+    #include <sys/ioctl.h>
+    #include <unistd.h>
+
+#elif defined(TERMINAL_TARGET_WINDOWS)
+    #include <io.h>
+    #include <windows.h>
+#endif
+
+#define LOGGER_ESCAPE_SEQUENCE "\033"
