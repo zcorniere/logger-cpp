8878427dcc86c3a9aa075ba8d1f45418d9adcdd6
Zacharie Corniere <zacharie@corniere.me>
2022-07-08 21:14:44 +0200

    chore(benchmark): add more message in benchmark

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -9,21 +9,28 @@
 
 constexpr auto file_path("file.txt");
 
-cpplogger::Logger logger(std::cout);
+cpplogger::Logger logger(std::clog);
 
 #define START_POINT(msg)                                    \
     auto start = std::chrono::high_resolution_clock::now(); \
-    file << msg " : " << start.time_since_epoch().count() << "s" << std::endl;
+    file << "end " msg " : " << start.time_since_epoch().count() << "s" << std::endl;
 
-#define END_POINT(msg)                                                       \
-    auto end = std::chrono::high_resolution_clock::now();                    \
-    file << msg " : " << end.time_since_epoch().count() << "s" << std::endl; \
-    std::chrono::duration<double> elapsedTime(end - start);                  \
+#define END_POINT(msg)                                                                \
+    auto end = std::chrono::high_resolution_clock::now();                             \
+    file << "start " msg " : " << end.time_since_epoch().count() << "s" << std::endl; \
+    std::chrono::duration<double> elapsedTime(end - start);                           \
     file << msg " took : " << elapsedTime.count() << "s" << std::endl;
 
-void print_bs() noexcept
+#define TEST_SCOPE(msg)   \
+    {                     \
+        START_POINT(msg); \
+        print_bs(msg);    \
+        END_POINT(msg);   \
+    }
+
+void print_bs(const std::string &msg) noexcept
 {
-    for (int i = 0; i < 1000000; i++) { logger.debug() << i; }
+    for (int i = 0; i < 1000000; i++) { logger.debug(msg) << i; }
 }
 
 int main(void)
@@ -31,22 +38,17 @@ int main(void)
     std::ofstream file((std::filesystem::path(file_path)));
     logger.start();
 
-    {
-        START_POINT("start 1000000");
-        print_bs();
-        END_POINT("end 1000000");
-    }
+    TEST_SCOPE("1000000")
 
     {
-        std::barrier sync_point(8, print_bs);
+        std::barrier sync_point(8, []() noexcept { print_bs("Multithread 1000000"); });
 
         auto work = [&sync_point]() { sync_point.arrive_and_wait(); };
 
         START_POINT("Multithread 1000000");
 
-        std::vector<std::thread> workers;
+        std::vector<std::jthread> workers;
         for (unsigned i = 0; i < 8; i++) { workers.emplace_back(work); }
-        for (auto &worker: workers) { worker.join(); }
 
         END_POINT("Multithread 1000000");
     }
