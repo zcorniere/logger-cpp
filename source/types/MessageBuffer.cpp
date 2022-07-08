a72d3a9743ca46ce24ab60812ebc7abbf33cd1b7
Zacharie Corniere <zacharie@corniere.me>
2022-07-08 16:42:59 +0200

    chore(MessageBuffer): better date output

--- source/types/MessageBuffer.cpp
+++ source/types/MessageBuffer.cpp
@@ -11,12 +11,9 @@ std::ostream &operator<<(std::ostream &os, const MessageBuffer &buffer)
         const std::time_t time = std::chrono::high_resolution_clock::to_time_t(buffer.now);
         auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(buffer.now.time_since_epoch()) -
                   std::chrono::duration_cast<std::chrono::seconds>(buffer.now.time_since_epoch());
-        auto micro = std::chrono::duration_cast<std::chrono::microseconds>(buffer.now.time_since_epoch()) -
-                     std::chrono::duration_cast<std::chrono::milliseconds>(buffer.now.time_since_epoch());
 
-        os << "[" << color(levelColor(buffer.level)) << std::put_time(std::localtime(&time), "%OM:%OS") << ":"
-           << std::setfill('0') << std::setw(3) << ms.count() << ":" << std::setfill('0') << std::setw(3)
-           << micro.count() << reset() << ']';
+        os << "[" << color(levelColor(buffer.level)) << std::put_time(std::localtime(&time), "%F %T") << ":"
+           << std::setfill('0') << std::setw(3) << ms.count() << reset() << ']';
         os << "[" << color(levelColor(buffer.level)) << to_string(buffer.level) << reset() << ']';
         if (!buffer.content->message.empty()) {
             os << "[" << color(levelColor(buffer.level)) << buffer.content->message << reset() << ']';
